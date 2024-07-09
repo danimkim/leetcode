6204a51f027628b96dcabb0915d0464dce3036a1
Danim Kim <74545780+danimkim@users.noreply.github.com>
2024-07-09 23:48:02 +0900

    refactor: refine the codes

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
@@ -1,15 +1,12 @@
 type Fn = (accum: number, curr: number) => number
 
 function reduce(nums: number[], fn: Fn, init: number): number {  
-    if(!nums.length){
-        return init;
-    }
+    // Store `init` in a new variable
+    let val = init;
 
-    let calculatedVal = 0;
-    nums.forEach((el, i) => {
-        let val = i === 0 ? init : calculatedVal;
-        calculatedVal = fn(val, el);
-    })
-
-    return calculatedVal;
+    // If `nums` array's length is 0, forEach loop will not execute any iteration.
+    // If the length of the `nums` array is greater than 0, in the first iteration of the loop, `val` will use the value of `init`
+    nums.forEach(el => fn(val, el));    
+    
+    return val;
 };
